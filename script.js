cce2b0d80bb1d4b55459af132f0963087ff2f4df
xpieemma <7chrispeter@gmail.com>
2026-01-16 08:09:51 -0500

    selected element by id from HTML

---
+++ script.js
@@ -0,0 +1,8 @@
+// selecting elements
+const form  = document.getElementById('registrationForm');
+const unsernameInput = document.getElementById('username');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const confirmPasswordInput = document.getElementById('confirmPassword');
+
+
